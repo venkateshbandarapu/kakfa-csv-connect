f019dd0bd1a6ee509e665cd5be121ac3533d44f7
venkateshbandarapu <venki.b321@gmail.com>
2020-05-22 13:55:13 +0530

    code refactor

--- src/main/java/com/csv/connector/CSVSourceTask.java
+++ src/main/java/com/csv/connector/CSVSourceTask.java
@@ -7,14 +7,12 @@ import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.util.*;
 import  static com.csv.connector.ConfigConstants.*;
+import static com.csv.connector.FileUtils.*;
 
 public class CSVSourceTask extends SourceTask {
 
@@ -31,83 +29,99 @@ public class CSVSourceTask extends SourceTask {
         this.mapper=new ObjectMapper();
         log.info("com.csv.connector.CSVSourceTask..start() method with :"+map);
     }
+    /*
+    actual task execution starts here..
+     */
 
     public List<SourceRecord> poll() throws InterruptedException {
-        Thread.sleep(11000);
+
         log.info("com.csv.connector.CSVSourceTask..poll() method");
         List<SourceRecord> processedRecords=new ArrayList<>();
        // this.context.offsetStorageReader().offset()
-        Iterator<File> files=getFilesList().iterator();
+        Iterator<File> files=getFilesListFromDir(connectionProps.get(INPUT_FILE_PATH),
+                connectionProps.get(INPUT_FILE_PATTERN)).iterator();
         while (files.hasNext()){
             File file=files.next();
-            List<SourceRecord> records= null;
-            try {
-                records = readCsvFile(file);
+            List<SourceRecord> records=getRecordsFromCsvFile(file);
+            if(records!=null){
                 processedRecords.addAll(records);
-                log.info("processed the records for file:"+file.getName()+" so moving this file to "+this.connectionProps.get(FINISHED_FILE_PATH));
-                FileUtils.MoveFile(file,this.connectionProps.get("finished.path"));
-            } catch (IOException e) {
-                log.error("unable to read/open the file:"+file.getName()+" so moving this file to "+this.connectionProps.get(ERROR_FILE_PATH));
-                FileUtils.MoveFile(file,this.connectionProps.get(ERROR_FILE_PATH));
-                e.printStackTrace();
             }
         }
         if(processedRecords.isEmpty()){
-            Thread.sleep(10000);
+            Thread.sleep(12000);
             return null;
         }
         return processedRecords;
     }
-
     public void stop() {
 
     }
-    public  List<File> getFilesList(){
 
-        List<File> csvFiles=new ArrayList<>();
-        File[] files=new File(this.connectionProps.get(INPUT_FILE_PATH)).listFiles();
+    public List<SourceRecord> getRecordsFromCsvFile(File file){
+        List<SourceRecord> records= null;
+        try {
+            records = readCsvFile(file);
+            log.info("processed the records for file:"+file.getName()+" so moving this file to "+this.connectionProps.get(FINISHED_FILE_PATH));
 
-        for (File file:files){
-            if(file.getName().matches(this.connectionProps.get(INPUT_FILE_PATTERN))){
-                csvFiles.add(file);
+        } catch (IOException|ArrayIndexOutOfBoundsException e) {
+            log.error("unable to process the file:"+file.getName()+" so moving this file to "+this.connectionProps.get(ERROR_FILE_PATH));
+            try {
+                MoveFile(file,this.connectionProps.get(ERROR_FILE_PATH));
+            } catch (IOException ex) {
+                log.error("unable to move the error file "+file+" to "+this.connectionProps.get(ERROR_FILE_PATH));
+                ex.printStackTrace();
+                return null;
             }
+            e.printStackTrace();
+            return null;
         }
-        return csvFiles;
+
+        try {
+            MoveFile(file,this.connectionProps.get("finished.path"));
+
+        } catch (IOException e) {
+            log.error("unable to move the processed file "+file+" to "+this.connectionProps.get(FINISHED_FILE_PATH));
+            log.error("so skipping the records of "+file+" to write into kafka topic");
+            e.printStackTrace();
+            return null;
+        }
+        return records;
 
     }
-    public List<SourceRecord> readCsvFile(File csvFile) throws IOException {
+    public List<SourceRecord> readCsvFile(File csvFile) throws IOException,ArrayIndexOutOfBoundsException {
 
         log.info("file name:"+csvFile);
         boolean headerProcessed=false;
 
         List<SourceRecord> records=new ArrayList<>();
-        CSVReader reader=new CSVReader(new FileReader(csvFile));
+         try(CSVReader reader=new CSVReader(new FileReader(csvFile))){
+             String[] row;//=reader.iterator();
 
-        String[] row;//=reader.iterator();
+             while ((row=reader.readNext())!=null){
+                 if (!headerProcessed){
+                     headerProcessed=true;
+                     continue;
+                 }
 
-        while ((row=reader.readNext())!=null){
-              if (!headerProcessed){
-                    headerProcessed=true;
-                    continue;
-              }
+                 Map<String,String> sourcePartition= new HashMap<>();
+                 sourcePartition.put("fileName",csvFile.getName());
 
-              Map<String,String> sourcePartition= new HashMap<>();
-              sourcePartition.put("fileName",csvFile.getName());
+                 Map<String,Long> sourceOffset= new HashMap<>();
+                 sourceOffset.put("offset",reader.getRecordsRead());
 
-              Map<String,Long> sourceOffset= new HashMap<>();
-              sourceOffset.put("offset",reader.getRecordsRead());
+                 Emp dto=new Emp(Integer.parseInt(row[0]),row[1],Double.parseDouble(row[2]));
 
-              Emp dto=new Emp(Integer.parseInt(row[0]),row[1],Double.parseDouble(row[2]));
+                 SourceRecord record=new SourceRecord(sourcePartition,sourceOffset,
+                         connectionProps.get(KAFKA_TOPIC_NAME),null,
+                         Schema.INT32_SCHEMA,dto.getEmp_id(),
+                         Schema.STRING_SCHEMA,convertObjectToString(dto));
+                 records.add(record);
 
-                    SourceRecord record=new SourceRecord(sourcePartition,sourceOffset,
-                            connectionProps.get(KAFKA_TOPIC_NAME),null,
-                            Schema.INT32_SCHEMA,dto.getEmp_id(),
-                            Schema.STRING_SCHEMA,convertObjectToString(dto));
-                    records.add(record);
+        }
+
                // }
-         }
+        }
 
-        reader.close();
 
         return records;
 
--- src/main/java/com/csv/connector/FileUtils.java
+++ src/main/java/com/csv/connector/FileUtils.java
@@ -9,13 +9,18 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.csv.connector.ConfigConstants.INPUT_FILE_PATH;
+import static com.csv.connector.ConfigConstants.INPUT_FILE_PATTERN;
 
 
 public class FileUtils {
 
    private static Logger log= LoggerFactory.getLogger(FileUtils.class);
 
-    public static void MoveFile(File source, String dest){
+    public static void MoveFile(File source, String dest) throws IOException{
         log.info("dest path:"+(dest+"/"+source.getName()));
             if(source.exists()){
                 if(source.renameTo(new File(dest+"/"+source.getName()))){
@@ -24,9 +29,23 @@ public class FileUtils {
                 else
                 {
                     log.error("moving of file from "+source+" to "+dest+" is failed");
+                    throw new IOException("unable to move the file from "+source+" to "+dest);
                 }
 
             }
+
+    }
+    public static List<File> getFilesListFromDir(String inputFilePath,String inputFilePattern){
+
+        List<File> csvFiles=new ArrayList<>();
+        File[] files=new File(inputFilePath).listFiles();
+
+        for (File file:files){
+            if(file.getName().matches(inputFilePattern)){
+                csvFiles.add(file);
+            }
+        }
+        return csvFiles;
 
     }
 }
